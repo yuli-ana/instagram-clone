16649bebc48fb47b5500568bd05de46b0529010a
Yuliana Hazda <yulianahazda@gmail.com>
2020-09-04 14:48:48 -0400

    Add content to the not-found page

--- src/components/shared/Layout.js
+++ src/components/shared/Layout.js
@@ -3,14 +3,14 @@ import { useLayoutStyles } from "../../styles";
 import Seo from "../shared/Seo";
 import Navbar from "../shared/Navbar";
 
-function Layout({ children, title }) {
+function Layout({ children, title, marginTop = 60 }) {
   const classes = useLayoutStyles();
 
   return (
     <section className={classes.section}>
       <Seo title={title} />
       <Navbar></Navbar>
-      <main className={classes.main}>
+      <main className={classes.main} style={{ marginTop }}>
         <section className={classes.childrenWrapper}>
           <div className={classes.children}>{children}</div>
         </section>
