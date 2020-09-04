dc08a99de615ce6187b19254633a0857c67d6509
Yuliana Hazda <yulianahazda@gmail.com>
2020-09-04 09:59:52 -0400

    Install prettier and eslint, add configurations

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div>app</div>;
+  return <Router>
+    
+  </Router>;
 }
 
 export default App;
