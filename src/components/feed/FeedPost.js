56172042d9c9081126edf2fbff8901519eb04a08
Yuliana Hazda <yulianahazda@gmail.com>
2020-09-08 21:29:25 -0400

    Distructure post, add icons and conditionally render likes

--- src/components/feed/FeedPost.js
+++ src/components/feed/FeedPost.js
@@ -1,10 +1,54 @@
 import React from "react";
 import { useFeedPostStyles } from "../../styles";
+import UserCard from "../shared/UserCard";
+import {
+  MoreIcon,
+  ShareIcon,
+  LikeIcon,
+  CommentIcon,
+  UnlikeIcon,
+  SaveIcon,
+} from "../../icons";
+import { Link } from "react-router-dom";
+import { Button, Typography } from "@material-ui/core";
 
-function FeedPost() {
-  useFeedPostStyles();
+function FeedPost({ post }) {
+  const classes = useFeedPostStyles();
 
-  return <div>FeedPost</div>;
+  const { media, id, caption, likes } = post;
+
+  return (
+    <>
+      <article className={classes.article}>
+        {/* Feed post header */}
+        <div className={classes.postHeader}>
+          <UserCard />
+          <MoreIcon className={classes.MoreIcon} />
+        </div>
+        {/* Feed post image */}
+        <div>
+          <img src={media} alt="Post media" className={classes.image} />
+        </div>
+        {/* Feed post image */}
+        <div className={classes.postButtonsWrapper}>
+          <div className={classes.postButtons}>
+            {/* <LikeButton /> */}
+            <Link to={`/p/:${id}`}>
+              <CommentIcon />
+            </Link>
+            <ShareIcon />
+            {/* <SaveButton /> */}
+          </div>
+          <Typography className={classes.like} variant="subtitle2">
+            <span>{likes === 1 ? "1 like" : `${likes} likes`}</span>
+          </Typography>
+        </div>
+      </article>
+    </>
+  );
 }
+
+// function LikeButton() {}
+// function SaveButton() {}
 
 export default FeedPost;
