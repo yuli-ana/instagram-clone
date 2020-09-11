2e23eb0b68aa289c9ca368dc494b0fa26f7f6544
Yuliana Hazda <yulianahazda@gmail.com>
2020-09-11 13:07:33 -0400

    Updates. add a tooltip

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useNavbarStyles } from "../../styles";
+import React, { useState, useEffect } from "react";
+import { useNavbarStyles, WhiteTooltip } from "../../styles";
 import { AppBar, Hidden, InputBase, Button, Avatar } from "@material-ui/core";
 import { Link, useHistory } from "react-router-dom";
 import logo from "../../images/logo.png";
@@ -14,7 +14,8 @@ import {
   HomeIcon,
 } from "../../icons";
 import NotificationList from "../notification/NotificationList";
-import { defaultCurrentUser } from "../../data";
+import { defaultCurrentUser, getDefaultUser } from "../../data";
+import { Grid } from "@material-ui/core";
 
 function Navbar({ minimalNavbar }) {
   const classes = useNavbarStyles();
@@ -53,24 +54,60 @@ function Logo() {
 function Search() {
   const classes = useNavbarStyles();
   const [query, setQuery] = useState("");
-  let loading = false;
+  const [loading, setLoading] = useState(false);
+  const [results, setResults] = useState([]);
+
+  // If it's a non-empty string👇🏻 && results isn't empty array, it it's a true then add a tooltip
+  const hasResults = Boolean(query) && results.length > 0;
+
+  // Check if query is not empty
+  useEffect(() => {
+    if (!query.trim()) return;
+
+    setResults(Array.from({ length: 5 }, () => getDefaultUser()));
+  }, [query]);
 
   return (
     <Hidden xsDown>
-      <InputBase
-        onChange={(e) => setQuery(e.target.value)}
-        className={classes.input}
-        placeholder="Search"
-        value={query}
-        startAdornment={<span className={classes.searchIcon} />}
-        endAdornment={
-          loading ? (
-            <LoadingIcon />
-          ) : (
-            <span onClick={() => setQuery("")} className={classes.clearIcon} />
+      <WhiteTooltip
+        arrow
+        interactive
+        TransitionComponent=""
+        open={hasResults}
+        title={
+          hasResults && (
+            <Grid className={classes.resultContainer} container>
+              {results.map((result) => (
+                <Grid key={result.id} item className={classes.resultLink}>
+                  <div className={classes.resultWrapper}>
+                    <div className={classes.avatarWrapper}>
+                      <Avatar src={result.profile_image} alt="User avatar" />
+                    </div>
+                  </div>
+                </Grid>
+              ))}
+            </Grid>
           )
         }
-      />
+      >
+        <InputBase
+          onChange={(e) => setQuery(e.target.value)}
+          className={classes.input}
+          placeholder="Search"
+          value={query}
+          startAdornment={<span className={classes.searchIcon} />}
+          endAdornment={
+            loading ? (
+              <LoadingIcon />
+            ) : (
+              <span
+                onClick={() => setQuery("")}
+                className={classes.clearIcon}
+              />
+            )
+          }
+        />
+      </WhiteTooltip>
     </Hidden>
   );
 }
@@ -79,32 +116,39 @@ function Links({ path }) {
   const classes = useNavbarStyles();
   const [showList, setList] = useState(false);
 
+  function handleToggleList() {
+    // Toggle state
+    setList((prev) => !prev);
+
+    return <div>{showList ? <NotificationList /> : ""}</div>;
+  }
+
   return (
     <div className={classes.linksContainer}>
-      <div className={classes.linksContainer}>
+      <div className={classes.linksWrapper}>
         <Hidden xsDown>
           <AddIcon />
-          <Link to="/">{path === "/" ? <HomeActiveIcon /> : <HomeIcon />}</Link>
-          <Link to="/explore">
-            {path === "/explore" ? <ExploreActiveIcon /> : <ExploreIcon />}
-          </Link>
-          <div className={classes.notifications}>
-            {showList ? <LikeActiveIcon /> : <LikeIcon />}
-          </div>
-          <Link to={`${defaultCurrentUser.username}`}>
-            <div
-              className={
-                path === `/${defaultCurrentUser.username}`
-                  ? classes.profileActive
-                  : ""
-              }
-            ></div>
-            <Avatar
-              src={defaultCurrentUser.profile_image}
-              className={classes.profileImage}
-            />
-          </Link>
         </Hidden>
+        <Link to="/">{path === "/" ? <HomeActiveIcon /> : <HomeIcon />}</Link>
+        <Link to="/explore">
+          {path === "/explore" ? <ExploreActiveIcon /> : <ExploreIcon />}
+        </Link>
+        <div className={classes.notifications} onClick={handleToggleList}>
+          {showList ? <LikeActiveIcon /> : <LikeIcon />}
+        </div>
+        <Link to={`${defaultCurrentUser.username}`}>
+          <div
+            className={
+              path === `/${defaultCurrentUser.username}`
+                ? classes.profileActive
+                : ""
+            }
+          ></div>
+          <Avatar
+            src={defaultCurrentUser.profile_image}
+            className={classes.profileImage}
+          />
+        </Link>
       </div>
     </div>
   );
--- src/data.js
+++ src/data.js
@@ -64,8 +64,7 @@ export const defaultCurrentUser = {
   id: uuid(),
   username: "me",
   name: "myself",
-  profile_image:
-    "https://scontent-ort2-2.cdninstagram.com/v/t51.2885-19/s150x150/82559664_3161307737426774_8687807477812559872_n.jpg?_nc_ht=scontent-ort2-2.cdninstagram.com&_nc_ohc=llb3VCRb-mkAX_XTEi3&oh=64f0323db646c01299e513e3337b83ff&oe=5E8620BF",
+  profile_image: "https://picsum.photos/id/1012/367/267",
   website: "https://react12.io",
   email: "me@gmail.com",
   bio: "This is my bio",
