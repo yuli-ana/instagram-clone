2741620796b62aed8ff2804b48f9c97177490d90
Yuliana Hazda <yulianahazda@gmail.com>
2020-09-16 13:15:38 -0400

    Update Slider props, create a new components, add user avatar, username and name to the card

--- src/components/shared/FollowSuggestions.js
+++ src/components/shared/FollowSuggestions.js
@@ -1,4 +1,4 @@
-import { Typography } from "@material-ui/core";
+import { Avatar, Typography } from "@material-ui/core";
 import React from "react";
 import { useFollowSuggestionsStyles } from "../../styles";
 import { Link } from "react-router-dom";
@@ -6,11 +6,13 @@ import { LoadingLargeIcon } from "../../icons";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { getDefaultUser } from "../../data";
+import FollowButton from "../shared/FollowButton";
 
 function FollowSuggestions() {
   const classes = useFollowSuggestionsStyles();
 
-  let loading = true;
+  let loading = false;
 
   return (
     <div className={classes.container}>
@@ -21,12 +23,70 @@ function FollowSuggestions() {
       >
         Suggestion For You
       </Typography>
-      {loading ? <LoadingLargeIcon /> : <Slider />}
       <Link>
         <Typography variant="subtitle2" color="primary">
           See All
         </Typography>
       </Link>
+      {loading ? (
+        <LoadingLargeIcon />
+      ) : (
+        <Slider
+          className={classes.slide}
+          dots={false}
+          infinite
+          speed={1000}
+          touchThreshold={1000}
+          variableWidth
+          swipeToSlide
+          arrows
+          slidesToScroll={3}
+          easing="ease-in-out"
+        >
+          {Array.from({ length: 10 }, () => getDefaultUser()).map((user) => (
+            <FollowSuggestionsItem key={user.id} user={user} />
+          ))}
+        </Slider>
+      )}
+    </div>
+  );
+}
+function FollowSuggestionsItem({ user }) {
+  const classes = useFollowSuggestionsStyles();
+  const { id, username, name, profile_image } = user;
+
+  return (
+    <div>
+      <div className={classes.card}>
+        <Link to={`/${username}`}>
+          <Avatar
+            src={profile_image}
+            alt={`${username}'s profile`}
+            classes={{
+              root: classes.avatar,
+              image: classes.avatarImg,
+            }}
+          />
+        </Link>
+        <Link to={`/${username}`}>
+          <Typography
+            variant="subtitle2"
+            className={classes.text}
+            align="center"
+          >
+            {username}
+          </Typography>
+        </Link>
+        <Typography
+          color="textSecondary"
+          variant="body2"
+          className={classes.text}
+          align="center"
+        >
+          {name}
+        </Typography>
+        <FollowButton side={false} />
+      </div>
     </div>
   );
 }
