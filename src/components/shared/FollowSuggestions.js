2cbc87ede1cb384623c3bfbf97713f3c9c8ad959
Yuliana Hazda <yulianahazda@gmail.com>
2020-09-16 12:40:54 -0400

    Import package react-slick, use Slider to add carousel

--- src/components/shared/FollowSuggestions.js
+++ src/components/shared/FollowSuggestions.js
@@ -1,10 +1,34 @@
+import { Typography } from "@material-ui/core";
 import React from "react";
 import { useFollowSuggestionsStyles } from "../../styles";
+import { Link } from "react-router-dom";
+import { LoadingLargeIcon } from "../../icons";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function FollowSuggestions() {
   const classes = useFollowSuggestionsStyles();
 
-  return <div>FollowSuggestions</div>;
+  let loading = true;
+
+  return (
+    <div className={classes.container}>
+      <Typography
+        variant="subtitle2"
+        color="textSecondary"
+        className={classes.typography}
+      >
+        Suggestion For You
+      </Typography>
+      {loading ? <LoadingLargeIcon /> : <Slider />}
+      <Link>
+        <Typography variant="subtitle2" color="primary">
+          See All
+        </Typography>
+      </Link>
+    </div>
+  );
 }
 
 export default FollowSuggestions;
