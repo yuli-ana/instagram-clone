fbd78a439d1a04c5e0cd377aa9e8d6804258a73e
Yuliana Hazda <yulianahazda@gmail.com>
2020-09-17 11:26:20 -0400

    Build post, add an overlay and comments/likes icons

--- src/components/shared/GridPost.js
+++ src/components/shared/GridPost.js
@@ -1,13 +1,24 @@
+import { Typography } from "@material-ui/core";
 import React from "react";
 import { useGridPostStyles } from "../../styles";
 
 function GridPost({ post }) {
   const classes = useGridPostStyles();
-  const { media } = post;
+  const { media, likes, comments } = post;
 
   return (
-    <div>
-      <img src={media} alt="" />
+    <div className={classes.gridPostContainer}>
+      <div className={classes.gridPostOverlay}>
+        <div className={classes.gridPostInfo}>
+          <span className={classes.likes} />
+          <Typography>{likes}</Typography>
+        </div>
+        <div className={classes.gridPostInfo}>
+          <span className={classes.comments} />
+          <Typography>{comments.length}</Typography>
+        </div>
+      </div>
+      <img src={media} alt="Post cover" className={classes.image} />
     </div>
   );
 }
