558027b3914c7da4eaacb8318451b07bd7d6df2a
Yuliana Hazda <yulianahazda@gmail.com>
2020-09-17 12:59:33 -0400

    Move Router to index.js to give useHistory access to the history data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route, useHistory } from "react-router-dom";
 import EditProfilePage from "./pages/edit-profile";
 import ExplorePage from "./pages/explore";
 import FeedPage from "./pages/feed";
@@ -11,19 +11,20 @@ import SignUpPage from "./pages/signup";
 import PostModal from "./components/post/PostModal";
 
 function App() {
+  const history = useHistory();
+  console.log(history);
+
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={FeedPage} />
-        <Route path="/explore" component={ExplorePage} />
-        <Route exact path="/:username" component={ProfilePage} />
-        <Route exact path="/p/:postId" component={PostPage} />
-        <Route path="/accounts/edit" component={EditProfilePage} />
-        <Route path="/accounts/login" component={LoginPage} />
-        <Route path="/accounts/emailsignup" component={SignUpPage} />
-        <Route path="*" component={NotFoundPage} />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route exact path="/" component={FeedPage} />
+      <Route path="/explore" component={ExplorePage} />
+      <Route exact path="/:username" component={ProfilePage} />
+      <Route exact path="/p/:postId" component={PostPage} />
+      <Route path="/accounts/edit" component={EditProfilePage} />
+      <Route path="/accounts/login" component={LoginPage} />
+      <Route path="/accounts/emailsignup" component={SignUpPage} />
+      <Route path="*" component={NotFoundPage} />
+    </Switch>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
 import theme from "./theme";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
     <CssBaseline />
-    <App />
+    <Router>
+      <App />
+    </Router>
   </MuiThemeProvider>,
   document.getElementById("root")
 );
