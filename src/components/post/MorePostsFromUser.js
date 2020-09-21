7af588e4acd4b0847d29d4037654cc80c151c48e
Yuliana Hazda <yulianahazda@gmail.com>
2020-09-21 14:54:18 -0400

    Updates, import defaultUser and styles

--- src/components/post/MorePostsFromUser.js
+++ src/components/post/MorePostsFromUser.js
@@ -1,10 +1,38 @@
 import React from "react";
 import { useMorePostsFromUserStyles } from "../../styles";
+import { getDefaultPost, defaultUser } from "../../data";
+import { LoadingLargeIcon } from "../../icons";
+import { Typography } from "@material-ui/core";
+import GridPost from "../shared/GridPost";
 
 function MorePostsFromUser() {
   const classes = useMorePostsFromUserStyles();
+  let loading = false;
 
-  return <div>MorePostsFromUser</div>;
+  return (
+    <>
+      <Typography
+        color="textSecondary"
+        variant="subtitle2"
+        component="h2"
+        gutterBottom
+        className={classes.typography}
+      >
+        Explore
+      </Typography>
+      {loading ? (
+        <LoadingLargeIcon />
+      ) : (
+        <article className={classes.article}>
+          <div className={classes.postContainer}>
+            {Array.from({ length: 20 }, () => getDefaultPost()).map((post) => (
+              <GridPost key={post.id} post={post} />
+            ))}
+          </div>
+        </article>
+      )}
+    </>
+  );
 }
 
 export default MorePostsFromUser;
