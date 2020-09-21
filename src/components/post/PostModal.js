ae1a36d982752c38b453437e499be888defa2a17
Yuliana Hazda <yulianahazda@gmail.com>
2020-09-21 11:22:36 -0400

    Import useParams hook, pass id prop to Post component

--- src/components/post/PostModal.js
+++ src/components/post/PostModal.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { usePostModalStyles } from "../../styles";
 import Modal from "react-modal";
-import { useHistory } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
+import Post from "./Post";
 
 function PostModal() {
   const history = useHistory();
+  const { postId } = useParams();
+  console.log(postId, "PARAMS");
   const classes = usePostModalStyles();
 
   return (
@@ -13,7 +16,26 @@ function PostModal() {
         isOpen
         overlayClassName={classes.overlay}
         onRequestClose={() => history.goBack()}
-      ></Modal>
+        style={{
+          content: {
+            display: "flex",
+            alignItems: "center",
+            maxWidth: 935,
+            width: "100%",
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            transform: "translate(-50%, -50%)",
+            margin: 0,
+            padding: 0,
+            overflow: "none",
+            WebkitOverflowScrolling: "touch",
+          },
+        }}
+      >
+        <Post id={postId} />
+      </Modal>
     </>
   );
 }
