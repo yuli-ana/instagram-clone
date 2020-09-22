2860cfdb66b905883c238295836c111587259d9f
Yuliana Hazda <yulianahazda@gmail.com>
2020-09-22 11:37:56 -0400

    Update metadata, import defaultUser

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { useProfilePageStyles } from "../styles";
 import Layout from "../components/shared/Layout";
+import { defaultCurrentUser } from "../data";
 
 function ProfilePage() {
   const classes = useProfilePageStyles();
 
-  return <Layout>ProfilePage</Layout>;
+  return (
+    <Layout
+      title={`${defaultCurrentUser.name} (@${defaultCurrentUser.username})}`}
+    ></Layout>
+  );
 }
 
 export default ProfilePage;
