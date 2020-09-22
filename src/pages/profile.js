5d8fa3960e9063a8aaeada3de4f9dbbc0ad4805d
Yuliana Hazda <yulianahazda@gmail.com>
2020-09-22 12:00:46 -0400

    Create local components, import profilePicture

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -2,7 +2,8 @@ import React from "react";
 import { useProfilePageStyles } from "../styles";
 import Layout from "../components/shared/Layout";
 import { defaultCurrentUser } from "../data";
-import { Hidden } from "@material-ui/core";
+import { Hidden, Card, CardContent } from "@material-ui/core";
+import ProfilePicture from "../components/shared/ProfilePicture";
 
 function ProfilePage() {
   const classes = useProfilePageStyles();
@@ -12,11 +13,32 @@ function ProfilePage() {
       title={`${defaultCurrentUser.name} (@${defaultCurrentUser.username})}`}
     >
       <div className={classes.container}>
-        <Hidden xsDown></Hidden>
+        <Hidden xsDown>
+          <Card>
+            <ProfilePicture />
+            <CardContent className={classes.cardContentLarge}>
+              <ProfileNameSection />
+              <PostCountSection />
+              <NameBioSection />
+            </CardContent>
+          </Card>
+        </Hidden>
         <Hidden smUp>hey</Hidden>
       </div>
     </Layout>
   );
+}
+
+function ProfileNameSection() {
+  return <>Profile Name Section</>;
+}
+
+function PostCountSection() {
+  return <>Post Count Section</>;
+}
+
+function NameBioSection() {
+  return <>Name Bio Section</>;
 }
 
 export default ProfilePage;
