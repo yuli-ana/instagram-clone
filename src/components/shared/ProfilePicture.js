b5bf4f9aadf452606b031ff067f3b80ebef621d6
Yuliana Hazda <yulianahazda@gmail.com>
2020-09-22 12:26:56 -0400

    Update, conditionally render user image, import a Person comp. from material UI

--- src/components/shared/ProfilePicture.js
+++ src/components/shared/ProfilePicture.js
@@ -1,10 +1,26 @@
 import React from "react";
 import { useProfilePictureStyles } from "../../styles";
+import { Person } from "@material-ui/icons";
 
-function ProfilePicture() {
-  const classes = useProfilePictureStyles();
+const img =
+  "https://i.picsum.photos/id/1005/367/267.jpg?hmac=bl_eyI1wwd6n-Q120mDottBNmCDNBurz7Z-b5IOeJU0";
 
-  return <div>ProfilePicture</div>;
+function ProfilePicture({ size, image = img, isOwner }) {
+  const classes = useProfilePictureStyles({ size, isOwner });
+
+  return (
+    <section className={classes.section}>
+      {image ? (
+        <div className={classes.wrapper}>
+          <img src={image} alt="user profile" className={classes.image} />
+        </div>
+      ) : (
+        <div className={classes.wrapper}>
+          <Person className={classes.person} />
+        </div>
+      )}
+    </section>
+  );
 }
 
 export default ProfilePicture;
