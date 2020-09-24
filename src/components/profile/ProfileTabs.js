97395322e9116da947f0b4290b5fa32c7b558822
Yuliana Hazda <yulianahazda@gmail.com>
2020-09-24 15:09:06 -0400

    Add a new state, import useState hook

--- src/components/profile/ProfileTabs.js
+++ src/components/profile/ProfileTabs.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useProfileTabsStyles } from "../../styles";
 import { Hidden, Divider, Tabs } from "@material-ui/core";
 
 function ProfileTabs({ isOwner, user }) {
   const classes = useProfileTabsStyles();
+  const [value, setValue] = useState(0);
 
   return (
     <>
