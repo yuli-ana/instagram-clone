791c794f19d9c943364832b559884f679eee5d04
Yuliana Hazda <yulianahazda@gmail.com>
2020-09-24 15:07:09 -0400

    Update JSX

--- src/components/profile/ProfileTabs.js
+++ src/components/profile/ProfileTabs.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { useProfileTabsStyles } from "../../styles";
+import { Hidden, Divider, Tabs } from "@material-ui/core";
 
 function ProfileTabs({ isOwner, user }) {
   const classes = useProfileTabsStyles();
 
-  return <div>ProfileTabs</div>;
+  return (
+    <>
+      <section className={classes.section}>
+        <Hidden xsDown>
+          <Divider />
+        </Hidden>
+        <Hidden xsDown>
+          <Tabs></Tabs>
+        </Hidden>
+      </section>
+    </>
+  );
 }
 
 export default ProfileTabs;
