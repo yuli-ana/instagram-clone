66cb43d25b942f9e1d4f61cde27c3b0cc6bea65e
Yuliana Hazda <yulianahazda@gmail.com>
2020-09-25 22:20:57 -0400

    Add event handler, toggle state

--- src/pages/edit-profile.js
+++ src/pages/edit-profile.js
@@ -1,12 +1,24 @@
 import React, { useState } from "react";
 import { useEditProfilePageStyles } from "../styles";
 import Layout from "../components/shared/Layout";
+import { IconButton } from "@material-ui/core";
 
 function EditProfilePage() {
   const classes = useEditProfilePageStyles();
   const [showDrawer, setDrawer] = useState(false);
 
-  return <Layout></Layout>;
+  function handleToggleDrawer() {
+    setDrawer((prev) => !prev);
+  }
+
+  return (
+    <Layout title="Edit profile">
+      <section className={classes.section}>
+        {/* MaterialUI */}
+        <IconButton edge="start"></IconButton>
+      </section>
+    </Layout>
+  );
 }
 
 export default EditProfilePage;
