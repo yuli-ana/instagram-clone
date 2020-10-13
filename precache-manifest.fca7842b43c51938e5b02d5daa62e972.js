2eda52fa445fa0f1acb0170bfb49b7a8280eb6e0
Yuliana Hazda <yulianahazda@gmail.com>
2020-10-12 20:11:58 -0400

    Updates

---
+++ precache-manifest.fca7842b43c51938e5b02d5daa62e972.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a7a07c6d1f69ae6d4bb1b18e483b42e",
+    "url": "/instagram-clone/index.html"
+  },
+  {
+    "revision": "92897be721c8a5c9bfbd",
+    "url": "/instagram-clone/static/css/2.2c3c496e.chunk.css"
+  },
+  {
+    "revision": "92897be721c8a5c9bfbd",
+    "url": "/instagram-clone/static/js/2.e0211d4b.chunk.js"
+  },
+  {
+    "revision": "e8ccc7cf4c855a56e3a7079a2b405db4",
+    "url": "/instagram-clone/static/js/2.e0211d4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09cfed96ffc9b349c521",
+    "url": "/instagram-clone/static/js/3.5d06a4bc.chunk.js"
+  },
+  {
+    "revision": "3b6f995d762269aed28f",
+    "url": "/instagram-clone/static/js/main.863c966a.chunk.js"
+  },
+  {
+    "revision": "66a42812131e953d63b8",
+    "url": "/instagram-clone/static/js/runtime-main.ca373e47.js"
+  },
+  {
+    "revision": "cbb9b56a5cf892318fc8ab502f9d3ba7",
+    "url": "/instagram-clone/static/media/facebook-icon-blue.cbb9b56a.svg"
+  },
+  {
+    "revision": "a9a13771e5dcd73931e0b61df2faac62",
+    "url": "/instagram-clone/static/media/icon-sheet-2.a9a13771.png"
+  },
+  {
+    "revision": "3dfb71b4bb35ca8b35728b9475b7d861",
+    "url": "/instagram-clone/static/media/icon-sheet.3dfb71b4.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/instagram-clone/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/instagram-clone/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/instagram-clone/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/instagram-clone/static/media/slick.f97e3bbf.svg"
+  }
+]);
